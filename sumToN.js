6bb421ffa2ca2de2f60f2cdb04874c814a1a00c7
Leo Ọládimú <82811737+leooladimu@users.noreply.github.com>
2022-11-02 13:58:05 -0400

    1

---
+++ sumToN.js
@@ -0,0 +1,55 @@
+ /***********************************************************************
+Write a recursive function called `sumToN` that takes in a number and returns
+the sum of all the numbers from 0 to that number. Return null for any input
+number below 0.
+
+Examples:
+
+sumToN(5) // returns 15
+sumToN(1)  // returns 1
+sumToN(9)  // returns 45
+sumToN(-8)  // returns null
+***********************************************************************/
+function sumToN(num) {
+
+  // one-liner: return num && num + sumToN(num - 1);
+
+  if (num < 0) {
+    return null;
+  }
+  if (num === 0) {
+    return 0;
+  }
+  return num + sumToN(num - 1)
+}
+
+
+// console.log(sumToN(5));
+
+// sumToN(5) // returns 15
+// sumToN(1)  // returns 1
+// sumToN(9)  // returns 45
+// sumToN(-8)  // returns null
+
+// 
+// function sumToN(n) {
+//   if (n.length  > 0) {
+//     console.log(n.toUpperCase());
+//     let answer = sumToN(n.slice(1))
+//     console.log('answer:');
+//     console.log(answer);
+//     console.log('1st n:');
+//     console.log(n);
+//     return n
+//   } else {
+//     console.log(n.toUpperCase());
+//     console.log('2nd n:');
+//     console.log(n);
+//     return n
+//   }
+// 
+// }
+// 
+// 
+// sumToN('numberString');
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
